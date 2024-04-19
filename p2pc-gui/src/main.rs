88456b40c6e498cb3aed33d2c712d90f471814cb
crazycraftix <crazycraftix@noreply.codeberg.org>
2024-04-19 11:08:15 +0200

    parse cli arguments

--- p2pc-gui/src/main.rs
+++ p2pc-gui/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser as _;
+
+/// Start or connect to an existing p2pc network
+#[derive(clap::Parser, Debug)]
+struct CliArguments {
+    /// Initial peers to connect to
+    #[arg(short, long, num_args(1..), value_name="MULTIADDRESS")]
+    peer_addresses: Vec<libp2p::Multiaddr>,
+
+    /// Interfaces to listen on
+    #[arg(short, long, num_args(1..), value_name="MULTIADDRESS", default_value = "/ip6/::/tcp/0")]
+    listen_addresses: Vec<libp2p::Multiaddr>,
+}
+
+#[tokio::main]
+async fn main() {
+    let args = CliArguments::parse();
+
+    println!("args: {:?}", args);
 }
